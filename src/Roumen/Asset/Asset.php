9cfd6651baf8ccd1fd608caae567902a366b8231
Roumen Damianoff <roumen@dawebs.com>
2013-07-17 20:43:27 +0300

    optimizing arrays

--- src/Roumen/Asset/Asset.php
+++ src/Roumen/Asset/Asset.php
@@ -4,47 +4,43 @@
  * Asset class for laravel4-assets package.
  *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 1.4
+ * @version 1.5
  * @link http://roumen.me/projects/laravel4-asset
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 class Asset
 {
 
-    // assets arrays
     protected static $css = array();
     protected static $styles = array();
-    protected static $js_header = array();
-    protected static $js_footer = array();
-    protected static $script_header = array();
-    protected static $script_footer = array();
-    protected static $script_ready = array();
-    protected static $assets_domain = '/'; // e.g. http://cdn.domain.tld/
+    protected static $js = array();
+    protected static $scripts = array();
+    protected static $domain = '/';
 
 
     /**
      * Check environment
      *
      * @return void
-     */
+    */
     public static function checkEnv()
     {
         $env = \App::environment();
 
         if ($env == 'local' || $env == 'testing')
         {
-            self::$assets_domain = '/';
+            self::$domain = '/';
         }
     }
 
     /**
-     * Set domain
+     * Set domain name
      *
      * @return void
-     */
+    */
     public static function setDomain($url)
     {
-        self::$assets_domain = $url;
+        self::$domain = $url;
     }
 
 
@@ -52,11 +48,11 @@ class Asset
      * Add new asset
      *
      * @param string $a
-     * @param string $position
+     * @param string $name
      *
      * @return void
-     */
-    public static function add($a, $position = 'footer')
+    */
+    public static function add($a, $name = 'footer')
     {
         if (preg_match("/\.css/i", $a))
         {
@@ -67,10 +63,7 @@ class Asset
         elseif (preg_match("/\.js/i", $a))
         {
             // js
-            if ($position == 'header')
-                self::$js_header[] = $a;
-            else
-                self::$js_footer[] = $a;
+            self::$js[$name][] = $a;
         }
     }
 
@@ -79,11 +72,11 @@ class Asset
      * Add new asset as first in its array
      *
      * @param string $a
-     * @param string $position
+     * @param string $name
      *
      * @return void
-     */
-    public static function addFirst($a, $position = 'footer')
+    */
+    public static function addFirst($a, $name = 'footer')
     {
         if (preg_match("/\.css/i", $a))
         {
@@ -94,10 +87,7 @@ class Asset
         elseif (preg_match("/\.js/i", $a))
         {
             // js
-            if ($position == 'header')
-                array_unshift(self::$js_header, $a);
-            else
-                array_unshift(self::$js_footer, $a);
+            array_unshift(self::$js[$name], $a);
         }
     }
 
@@ -106,18 +96,13 @@ class Asset
      * Add new script
      *
      * @param string $s
-     * @param string $position
+     * @param string $name
      *
      * @return void
-     */
-    public static function addScript($s, $position = 'footer')
+    */
+    public static function addScript($s, $name = 'footer')
     {
-        if ($position == 'footer')
-            self::$script_footer[] = $s;
-        elseif ($position == 'header')
-            self::$script_header[] = $s;
-        else
-            self::$script_ready[] = $s;
+        self::$scripts[$name][] = $s;
     }
 
 
@@ -127,7 +112,7 @@ class Asset
      * @param string $s
      *
      * @return void
-     */
+    */
     public static function addStyle($s)
     {
         self::$style[] = $s;
@@ -136,7 +121,9 @@ class Asset
 
     /**
      * Loads all items from $css array
-     */
+     *
+     * @return void
+    */
     public static function css()
     {
         self::checkEnv();
@@ -150,7 +137,7 @@ class Asset
                     $url = $file;
                 } else
                     {
-                        $url = self::$assets_domain . $file;
+                        $url = self::$domain . $file;
                     }
                 echo '<link rel="stylesheet" type="text/css" href="' . $url . '" />' . "\n";
             }
@@ -162,7 +149,9 @@ class Asset
      * Loads all items from $styles array
      *
      * @param string $s
-     */
+     *
+     * @return void
+    */
     public static function styles($s)
     {
         if (!empty(self::$styles))
@@ -179,86 +168,65 @@ class Asset
 
 
     /**
-     * Loads all items from $js_header or $js_footer arrays
+     * Loads items from $js array
      *
-     * @param string $p (options: 'footer', 'header')
-     */
-    public static function js($p = 'footer')
+     * @param string $name
+     *
+     * @return void
+    */
+    public static function js($name = 'footer')
     {
         self::checkEnv();
 
-        if ($p == 'header')
+        if (!empty(self::$js[$name]))
         {
-            if (!empty(self::$js_header))
+            foreach(self::$js[$name] as $file)
             {
-                foreach(self::$js_header as $file)
+                if (stripos($file, "http://") === 0)
                 {
-                    if (stripos($file, "http://") === 0)
+                    $url = $file;
+                } else
                     {
-                        $url = $file;
-                    } else
-                        {
-                           $url = self::$assets_domain . $file;
-                        }
-                    echo '<script src="' . $url . '"></script>' . "\n";
-                }
-            }
-        } else {
-            if (!empty(self::$js_footer))
-            {
-                foreach(self::$js_footer as $file)
-                {
-                    if (stripos($file, "http://") === 0)
-                    {
-                        $url = $file;
-                    } else
-                        {
-                           $url = self::$assets_domain . $file;
-                        }
-                    echo '<script src="' . $url . '"></script>' . "\n";
-                }
+                       $url = self::$domain . $file;
+                    }
+                echo '<script src="' . $url . '"></script>' . "\n";
             }
         }
+
     }
 
 
     /**
-     * Loads all items from $scripts_header, $scripts_footer or $scripts_ready arrays
+     * Loads items from $scripts array
      *
-     * @param string $p (options: 'footer', 'header' or 'ready')
-     */
-    public static function scripts($p = 'footer')
+     * @param string $name
+     *
+     * @return void
+    */
+    public static function scripts($name = 'footer')
     {
-        if ($p == 'footer')
+        if ($name == 'ready')
         {
-            if (!empty(self::$script_footer))
-            {
-                foreach(self::$script_footer as $script)
-                {
-                    echo '<script>' . $script . "</script>\n";
-                }
-            }
-        } elseif ($p == 'header')
-        {
-            if (!empty(self::$script_header))
-            {
-                foreach(self::$script_header as $script)
-                {
-                    echo '<script>' . $script . "</script>\n";
-                }
-            }
-        } else {
-            if (!empty(self::$script_ready))
+            if (!empty(self::$scripts['ready']))
             {
                 $p = '<script>$(document).ready(function(){';
-                foreach(self::$script_ready as $script)
+                foreach(self::$scripts['ready'] as $script)
                 {
                     $p .= $script . "\n";
                 }
                 $p .= "});</script>\n";
                 echo $p;
             }
-        }
+        } else
+            {
+                if (!empty(self::$scripts[$name]))
+                {
+                    foreach(self::$scripts[$name] as $script)
+                    {
+                        echo '<script>' . $script . "</script>\n";
+                    }
+                }
+            }
     }
 
 
