689247f9faf935e5e6d30d0434605818d9236745
Roumen Damianoff <roumen@dawebs.com>
2013-09-06 21:50:45 +0300

    updated url

--- src/Roumen/Asset/Asset.php
+++ src/Roumen/Asset/Asset.php
@@ -4,8 +4,8 @@
  * Asset class for laravel4-assets package.
  *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 2.1
- * @link http://roumen.me/projects/laravel4-assets
+ * @version 2.2
+ * @link http://roumen.it/projects/laravel4-assets
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 class Asset
