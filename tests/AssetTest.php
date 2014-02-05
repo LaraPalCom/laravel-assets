cf6e5f5ddb2c5f062450af923e1b51a5c87be406
Roumen Damianoff <roumen@dawebs.com>
2014-02-05 14:06:54 +0200

    added AssetTest.php

---
+++ tests/AssetTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class AssetTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    // always pass
+    public function testTest()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
